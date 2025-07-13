3eed349005c3bee80a789cbe0f7fa94d498da08d
aimovementgit <recoveraimovement@gmail.com>
2025-07-13 10:50:19 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div className="min-h-screen bg-gray-50 py-8">
-        <div className="max-w-4xl mx-auto ">
+        <div className="max-w-4xl mx-auto md:px-4">
           <div className="bg-white rounded-lg shadow-lg p-8">
             <div className="text-center mb-8">
               <h1 className="text-3xl font-bold text-blue-950 mb-2">
